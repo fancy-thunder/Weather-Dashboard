0922c5d998906659e0ee3ecc89e74af37e0beed3
Avishek Sen <avi.aakash4u@gmail.com>
2025-04-09 05:18:47 +0530

    Adding the searchbar and submit button component
    Adding the navbar component which contains a search bar and a submit button

---
+++ weather-dashboard-app/src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+        <div className="nav-main">
+          <input type="text" placeholder="Search the city"></input>
+          <button>Search</button>
+        </div>
+    </>
+  )
+}
+
+export default Navbar
