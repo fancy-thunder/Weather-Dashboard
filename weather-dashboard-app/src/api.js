49461c0f7b1540e27142a144727908309e161010
Avishek Sen <avi.aakash4u@gmail.com>
2025-04-09 05:49:56 +0530

    Making api.js
    Putting my api key in the api.js and fetching the values and showing an error message if values and being unabled to fetched

---
+++ weather-dashboard-app/src/api.js
@@ -0,0 +1,19 @@
+import axios from "axios"
+
+const myAPI = "4cffd463bc560fdc8ab8857670ceb56e"
+
+const getWeather = async(city) =>{
+    try{
+        const result = await axios.get("https://api.openweathermap.org/data/2.5/weather",
+        {
+            params: {
+                apiId: myAPI,
+                city: city,
+                units: "metric"
+            },
+        })
+        return result.data
+    }catch (error){
+        <h1>There has been an error in fetching the data.. Try again</h1>
+    }
+}
